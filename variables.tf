30e8e7ce1668be72c62a3997f5fca16616bd48a7
Gaetano Carlucci <gaetano.carlucci@gmail.com>
2021-12-16 18:48:00 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "ikswsname" {
   type = string
   default = "DEVNET-1291"
 }
+variable "endpoint" {
+  type        = string
+  description = "API Endpoint URL"
+  default     = "https://www.intersight.com"
+}
