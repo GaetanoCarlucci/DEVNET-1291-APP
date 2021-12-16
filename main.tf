7902315d59c27b8db9d7de3544f332b0de90411d
Gaetano Carlucci <gaetano.carlucci@gmail.com>
2021-12-16 18:34:26 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,15 +24,15 @@ resource helm_release helloiksfrtfcb {
   }
 }
 
-provider "helm" {
-  kubernetes {
-    host = local.kube_config.clusters[0].cluster.server
-    client_certificate = base64decode(local.kube_config.users[0].user.client-certificate-data)
-    client_key = base64decode(local.kube_config.users[0].user.client-key-data)
-    cluster_ca_certificate = base64decode(local.kube_config.clusters[0].cluster.certificate-authority-data)
-  }
+data "intersight_kubernetes_cluster" "kubeconfig" {
+  name = var.cluster_names["iksterraformk8scluster"]
 }
 
-locals {
-  kube_config = yamldecode(data.terraform_remote_state.iksws.outputs.kube_config)
+provider "helm" {
+  kubernetes {
+    host = yamldecode(base64decode(data.intersight_kubernetes_cluster.kubeconfig.results[0].kube_config)).clusters[0].cluster.server
+    client_certificate = base64decode(yamldecode(base64decode(data.intersight_kubernetes_cluster.kubeconfig.results[0].kube_config)).users[0].user.client-certificate-data)
+    client_key = base64decode(yamldecode(base64decode(data.intersight_kubernetes_cluster.kubeconfig.results[0].kube_config)).users[0].user.client-key-data)
+    cluster_ca_certificate = base64decode(yamldecode(base64decode(data.intersight_kubernetes_cluster.kubeconfig.results[0].kube_config)).clusters[0].cluster.certificate-authority-data)
+  }
 }
