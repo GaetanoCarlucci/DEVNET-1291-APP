86dcae656a247ce3eb75df4114d847e8671b7eaf
Gaetano Carlucci <gaetano.carlucci@gmail.com>
2021-12-16 13:20:53 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,6 +9,9 @@ data "terraform_remote_state" "iksws" {
   }
 }
 
+output "iksws" {
+  value = "${data.terraform_remote_state}"
+  
 resource helm_release helloiksfrtfcb {
   name       = "helloiksapp"
   namespace = "default"
